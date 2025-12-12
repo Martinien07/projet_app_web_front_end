2a283aabc3afac650cf07af911a0bc40eacc6d61
tareq <tareqouedraogo2@gmail.com>
2025-12-11 19:21:46 -0500

    Ajout des routes notification

--- routes/notificationRoutes.js
+++ routes/notificationRoutes.js
@@ -3,35 +3,26 @@ import {
   createNotification,
   getNotifications,
   deleteNotification,
-  markNotificationAsRead, getNotificationById
-} from "../controllers/notificationController.js";
-import { protect } from "../middlewares/authMiddleware.js";
+  markNotificationAsRead,
+  getNotificationById
+} from "../controllers/notification.controller.js";
 
 const router = express.Router();
 
-/**
- * Créer une notification
- */
-router.post("/",protect, createNotification);
+// créer une notification
+router.post("/create", createNotification);
 
-/**
- * Récupérer les notifications avec filtres
- * Ex : GET /api/notification?target=18&scope=user&level=all&isRead=false
- */
-router.get("/",protect, getNotifications);
+// lister les notifications de l'utilisateur connecté
+router.get("/", getNotifications);
 
+// récupérer une notification par id
+router.get("/:id", getNotificationById);
 
-router.get("/:id",protect, getNotificationById);
+// marquer comme lue
+router.patch("/:notificationId/read", markNotificationAsRead);
 
-
-/**
- * Supprimer une notification
- */
-router.delete("/:id",protect, deleteNotification);
-
-/**
- * Marquer une notification comme lue
- */
-router.put("/read/:notificationId",protect, markNotificationAsRead);
+// supprimer une notification
+router.delete("/:id", deleteNotification);
 
 export default router;
+gi
